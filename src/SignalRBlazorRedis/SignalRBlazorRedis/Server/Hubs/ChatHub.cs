94ee378ac0ffeabd14c4874fbead5cd1aaee33d2
Serajoddin Aliabadi <serajoddin.aliabadi@yahoo.com>
2021-06-09 20:17:50 +0430

    Add Project

---
+++ src/SignalRBlazorRedis/SignalRBlazorRedis/Server/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+
+namespace SignalRBlazorRedis.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly IConfiguration _configuration;
+
+        public ChatHub(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendToAll(string message)
+            => await Clients.All.SendAsync(
+                "ShowMessageInClient"
+                , $"{message} - Send By Server : {_configuration.GetValue<string>("ServerName")}");
+    }
+}
